bdf01b631a481ba4587b6c6010a2c5dddcac0ffb
Ripan choudhary <bajwaripanpajal@gmail.com>
2021-09-14 10:10:09 +0530

    updates

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -16,7 +16,7 @@ export default function ErrorPage() {
             <h2>Page not found!</h2>
             <Button
                 variant="primary"
-                onClick={() => (window.location.href = "/learn_react")}>
+                onClick={() => (window.location.href = "/learning_react")}>
                 Go to Homepage!
             </Button>
         </Container>
